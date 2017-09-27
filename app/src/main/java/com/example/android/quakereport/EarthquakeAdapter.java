c9fbaa1118d2285e0772d094ee9a31ef2ca4426c
Abhas <abhas1102@gmail.com>
2017-09-27 23:03:33 +0530

    Magnitude is now surrounded by colored circle .

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.DecimalFormat;
+import android.graphics.drawable.GradientDrawable;
 
 /**
  * Created by Abhas on 25-09-2017.
@@ -26,6 +28,8 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         super(context,0, earthquakes);
 
     }
+
+
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -56,6 +60,20 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         String formattedMagnitude = formatMagnitude(currentEarthquake.getMagnitude());
         // Display the magnitude of the current earthquake in that TextView
         magnitudeView.setText(formattedMagnitude);
+
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+        // Get the original location string from the Earthquake object,
+        // which can be in the format of "5km N of Cairo, Egypt" or "Pacific-Antarctic Ridge".
+        originalLocation = currentEarthquake.getLocation();
+
+
+
 
         TextView primaryLocationView = (TextView) listItemView.findViewById(R.id.primary_location);
         primaryLocationView.setText(primaryLocation);
@@ -98,5 +116,54 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     private String formatMagnitude(double magnitude) {
         DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
         return magnitudeFormat.format(magnitude);
+    }
+
+    private int getMagnitudeColor(double magnitude){
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch(magnitudeFloor) {
+
+            case 0:
+
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 }
