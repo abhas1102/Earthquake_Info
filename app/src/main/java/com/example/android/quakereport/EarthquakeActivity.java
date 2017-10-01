ade453f7596dd638f4051f2c62fefe86583fdffb
Abhas <abhas1102@gmail.com>
2017-10-02 01:21:44 +0530

    Fetched data directly through provided link of API using HTTP request, AsyncTask and Json parsing

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -33,7 +33,7 @@ public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10&starttime=2017-01-01";
 
     private EarthquakeAdapter mAdapter;
 
@@ -79,40 +79,40 @@ public class EarthquakeActivity extends AppCompatActivity {
         task.execute(USGS_REQUEST_URL);
     }
 
-private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
 
-    /**
-     * This method runs on a background thread and performs the network request.
-     * We should not update the UI from a background thread, so we return a list of
-     * {@link Earthquake}s as the result.
-     */
-    @Override
-    protected List<Earthquake> doInBackground(String... urls) {
-        // Don't perform the request if there are no URLs, or the first URL is null
-        if (urls.length < 1 || urls[0] == null) {
-            return null;
+        /**
+         * This method runs on a background thread and performs the network request.
+         * We should not update the UI from a background thread, so we return a list of
+         * {@link Earthquake}s as the result.
+         */
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
         }
 
-        List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
-        return result;
-    }
+        /**
+         * This method runs on the main UI thread after the background work has been
+         * completed. This method receives as input, the return value from the doInBackground()
+         * method. First we clear out the adapter, to get rid of earthquake data from a previous
+         * query to USGS. Then we update the adapter with the new list of earthquakes,
+         * which will trigger the ListView to re-populate its list items.
+         */
+        @Override
+        protected void onPostExecute(List<Earthquake> data) {
+            // Clear the adapter of previous earthquake data
+            mAdapter.clear();
 
-    /**
-     * This method runs on the main UI thread after the background work has been
-     * completed. This method receives as input, the return value from the doInBackground()
-     * method. First we clear out the adapter, to get rid of earthquake data from a previous
-     * query to USGS. Then we update the adapter with the new list of earthquakes,
-     * which will trigger the ListView to re-populate its list items.
-     */
-    @Override
-    protected void onPostExecute(List<Earthquake> data) {
-        // Clear the adapter of previous earthquake data
-        mAdapter.clear();
-
-        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
-        // data set. This will trigger the ListView to update.
-        if (data != null && !data.isEmpty()) {
-            mAdapter.addAll(data);
+            // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
         }
     }
 }
--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.quakereport;
 
 import android.text.TextUtils;
@@ -19,84 +34,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Abhas on 27-09-2017.
+ * Helper methods related to requesting and receiving earthquake data from USGS.
  */
-
 public final class QueryUtils {
 
+    /** Tag for the log messages */
     private static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
-
+    /**
+     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+    private QueryUtils() {
+    }
 
     /**
-     * Helper methods related to requesting and receiving earthquake data from USGS.
+     * Query the USGS dataset and return a list of {@link Earthquake} objects.
      */
+    public static List<Earthquake> fetchEarthquakeData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
 
-
-        /** Sample JSON response for a USGS query */
-
-        /**
-         * Create a private constructor because no one should ever create a {@link QueryUtils} object.
-         * This class is only meant to hold static variables and methods, which can be accessed
-         * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
-         */
-        private QueryUtils() {
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
         }
 
-        /**
-         * Return a list of {@link Earthquake} objects that has been built up from
-         * parsing a JSON response.
-         public static List<Earthquake> fetchEarthquakeData(String requestUrl) {
-         // Create URL object
-         URL url = createUrl(requestUrl);
+        // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
+        List<Earthquake> earthquakes = extractFeatureFromJson(jsonResponse);
 
-         // Perform HTTP request to the URL and receive a JSON response back
-         String jsonResponse = null;
-         try {
-         jsonResponse = makeHttpRequest(url);
-         } catch (IOException e) {
-         Log.e(LOG_TAG, "Problem making the HTTP request.", e);
-         }
+        // Return the list of {@link Earthquake}s
+        return earthquakes;
+    }
 
-         // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
-         List<Earthquake> earthquakes = extractFeatureFromJson(jsonResponse);
-
-         // Return the list of {@link Earthquake}s
-         return earthquakes;
-         }
-
-
-         /**
-         * Returns new URL object from the given string URL.
-         */
-
-        public static List<Earthquake> fetchEarthquakeData(String requestUrl) {
-            //Create URL object
-            URL url = createUrl(requestUrl);
-
-            // Perform HTTP request to the URL and receive a JSON response back
-            String jsonResponse = null;
-            try {
-                jsonResponse = makeHttpRequest(url);
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Problem making the HTTP request.", e);
-            }
-
-            // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
-            List<Earthquake> earthquakes = extractFeatureFromJson(jsonResponse);
-
-            // Return the list of {@link Earthquake}s
-            return earthquakes;
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
         }
-        private static URL createUrl(String stringUrl) {
-            URL url = null;
-            try {
-                url = new URL(stringUrl);
-            } catch (MalformedURLException e) {
-                Log.e(LOG_TAG, "Problem building the URL ", e);
-            }
-            return url;
-        }
+        return url;
+    }
 
     /**
      * Make an HTTP request to the given URL and return a String as the response.
@@ -226,7 +212,5 @@ public final class QueryUtils {
         // Return the list of earthquakes
         return earthquakes;
     }
-
-
 
 }
